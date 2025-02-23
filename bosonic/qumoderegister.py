5e04ac71ff7cad73d5094106f5f06af323905740
PhyTensor <phystroq@gmail.com>
2025-02-23 22:23:45 +0300

    add: qumode register

---
+++ bosonic/qumoderegister.py
@@ -0,0 +1,21 @@
+from qiskit import QuantumRegister
+
+class QumodeRegister:
+    def __init__(self, num_qumodes: int, qubits_per_mode: int = 2, name: str = "q") -> None:
+        """
+        Initialise a QumodeRegister object.
+
+        Parameters:
+            num_qumodes (int): The number of qumodes in the register.
+            qubits_per_mode (int): The number of qubits representing each qumode. Defaults to 2.
+            name (str): The name of the register. Defaults to "q".
+        """
+        self.num_qumodes: int = num_qumodes
+        self.qubits_per_mode: int = qubits_per_mode
+        self.name: str = name
+
+        self.size: int = self.num_qumodes * self.qubits_per_mode
+        self.cutoff: int = 2**self.qubits_per_mode
+
+        self.quantum_register: QuantumRegister = QuantumRegister(size=self.size, name=self.name)
+
